2204c4e87bf0884ee8d582659e34e564d5ea8e3f
Ishiyuka <shijing05youjia@gmail.com>
2022-11-16 14:49:49 +0900

    root pathの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'sessions#new'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show, :edit, :update]
   resources :pictures do
@@ -6,7 +7,6 @@ Rails.application.routes.draw do
       post :confirm
     end
   end
-  root to: 'users#new'
   resources :favorites, only: [:index, :create, :destroy]
 
   mount LetterOpenerWeb::Engine, at: "/letter_opener"
